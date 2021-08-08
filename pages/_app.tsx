76f60d8954e78ea05a0b56a02c5aad3f8194e2fb
Daniel Jin <danieljin123@outlook.com>
2021-08-08 19:19:24 +0300

    Update _app & _document file to fix MUI issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
@@ -5,6 +6,14 @@ import store from '../store';
 import MainLayout from '../components/layouts/MainLayout';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles?.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
+
   return (
     <Provider store={store}>
       <MainLayout>
