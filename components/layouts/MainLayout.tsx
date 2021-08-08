2aced0b2e05c8cf992f67e58adc0f12d1f54978a
Daniel Jin <danieljin123@outlook.com>
2021-08-08 19:32:07 +0300

    Configure main layout

--- components/layouts/MainLayout.tsx
+++ components/layouts/MainLayout.tsx
@@ -1,20 +1,40 @@
-import { CssBaseline, ThemeProvider } from '@material-ui/core';
+import React from 'react';
+import { Container, CssBaseline, ThemeProvider, Box, makeStyles } from '@material-ui/core';
 import { GlobalStateProvider } from '../../contexts/GlobalStateContext';
 import theme from '../../theme';
 import Navbar from './Navbar';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  container: {
+    flex: '1',
+  },
+}));
 
 export interface MainLayoutProps {
   children: JSX.Element[] | JSX.Element;
 }
 
-const MainLayout: React.FC<MainLayoutProps> = ({ children }) => (
-  <GlobalStateProvider>
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Navbar />
-      {children}
-    </ThemeProvider>
-  </GlobalStateProvider>
-);
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  const classes = useStyles();
+
+  return (
+    <GlobalStateProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className={classes.root}>
+          <Navbar />
+          <Container className={classes.container}>
+            <Box my={10}>{children}</Box>
+          </Container>
+        </div>
+      </ThemeProvider>
+    </GlobalStateProvider>
+  );
+};
 
 export default MainLayout;
