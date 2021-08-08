d1a2479c44415727657c4810608b7df01c565ae5
Daniel Jin <danieljin123@outlook.com>
2021-08-08 22:31:42 +0300

    Update loading screen

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
 import { HashLoader } from 'react-spinners';
 
+const useStyles = makeStyles({
+  container: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+  },
+});
 export interface LoadingProps {
   loading: boolean;
 }
 
-const Loading: React.FC<LoadingProps> = ({ loading }) => (
-  <HashLoader color='#36D7B7' loading={loading} size={100} />
-);
+const Loading: React.FC<LoadingProps> = ({ loading }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.container}>
+      <HashLoader color='#36D7B7' loading={loading} size={100} />
+    </div>
+  );
+};
 
 export default Loading;
