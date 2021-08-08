a4e3bc599262743a60f5c6b9d9f53e5387c4894d
Daniel Jin <danieljin123@outlook.com>
2021-08-08 16:44:22 +0300

    Add Navbar

---
+++ components/layouts/Navbar.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Link from 'next/link';
+import { AppBar, Toolbar } from '@material-ui/core';
+import Logo from '../Logo';
+
+const AppNavbar = () => {
+  return (
+    <AppBar elevation={0}>
+      <Toolbar>
+        <Link href='/'>
+          <a>
+            <Logo />
+          </a>
+        </Link>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default AppNavbar;
